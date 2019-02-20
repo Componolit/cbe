b6498d8e3b70867f53900d35b673df85db02a766
Johannes Kliemann <kliemann@componolit.com>
2019-02-20 14:39:14 +0100

    c++ block test with generic interface

---
+++ src/cbe/test/block_client/cxx_block_test.h
@@ -0,0 +1,14 @@
+
+#include <block.h>
+
+#include <block_session/connection.h>
+
+class Cxx_block_test
+{
+    private:
+        Block::Client _block;
+        char _buffer[Block::Client::BLOCK_SIZE];
+    public:
+        Cxx_block_test(Block::Connection &conn);
+        void run();
+};
