61019958d158704f4337d4293fec6e6281b4d014
Johannes Kliemann <kliemann@componolit.com>
2019-02-27 14:01:33 +0100

    use device dependent block size

--- src/cbe/test/block_client/ada_block_test.adb
+++ src/cbe/test/block_client/ada_block_test.adb
@@ -2,6 +2,7 @@ with Ada.Unchecked_Conversion;
 with Block;
 with Block.Client;
 with Gnat.Io;
+use all type Block.Size;
 use all type Block.Client.Request_Kind;
 use all type Block.Client.Request_Status;
 
@@ -9,41 +10,45 @@ package body Ada_Block_Test is
 
    procedure Run is
       Client : Block.Client.Device := Block.Client.Create_Device;
-      subtype Block_Buffer is Block.Buffer (1 .. 512);
-      subtype Block_String is String (1 .. 512);
+      subtype Block_Buffer is Block.Buffer (1 .. 4096);
+      subtype Block_String is String (1 .. Block_Buffer'Length);
       function Convert_Block is new Ada.Unchecked_Conversion (Block_Buffer, Block_String);
-      Buf : Block_Buffer;
+      Buf : Block_Buffer := (others => 0);
       Write_Req : Block.Client.Request (Kind => Block.Client.Write);
       Read_Req : Block.Client.Request (Kind => Block.Client.Read);
-      Started : Boolean := False;
-      Handled : Boolean := False;
+      Acknowledged_Blocks : Integer;
+      Block_Size : Block.Size;
    begin
       Block.Client.Initialize_Device (Client, "");
+      Block_Size := Block.Client.Block_Size (Client);
+      if Block_Size > Block_Buffer'Length then
+         Gnat.Io.Put_Line ("ERROR: Block size too big");
+      end if;
+      Gnat.Io.Put_Line ("Writing...");
       Write_Req.Start := 1;
       Write_Req.Length := 1;
-      Buf := (others => Block.Byte (Character'Pos('a')));
-      Block.Client.Submit_Write (Client, Write_Req, Buf);
+      Buf (1 .. Block.Unsigned_Long (Block_Size)) := (others => Block.Byte (Character'Pos('a')));
+      Block.Client.Submit_Write (Client, Write_Req, Buf (1 .. Block.Unsigned_Long (Block_Size)));
       Write_Req.Start := 3;
-      Block.Client.Submit_Write (Client, Write_Req, Buf);
-      Buf := (others => Block.Byte (Character'Pos('d')));
+      Block.Client.Submit_Write (Client, Write_Req, Buf (1 .. Block.Unsigned_Long (Block_Size)));
+      Buf (1 .. Block.Unsigned_Long (Block_Size)) := (others => Block.Byte (Character'Pos('d')));
       Write_Req.Start := 2;
-      Block.Client.Submit_Write (Client, Write_Req, Buf);
-      loop
+      Block.Client.Submit_Write (Client, Write_Req, Buf (1 .. Block.Unsigned_Long (Block_Size)));
+      Acknowledged_Blocks := 0;
+      while Acknowledged_Blocks < 3 loop
          declare
-            Req : constant Block.Client.Request := Block.Client.Next (Client);
+            Req : Block.Client.Request := Block.Client.Next (Client);
          begin
-            started := started or Req.Kind /= Block.Client.None;
-            handled := started and Req.Kind = Block.Client.None;
-            if handled then
-               Gnat.Io.Put_Line ("writing finished");
-            end if;
-            if started and not handled and Req.Kind = Block.Client.Write then
+            if Req.Kind = Block.Client.Write then
+               Acknowledged_Blocks := Acknowledged_Blocks + 1;
                Gnat.Io.Put_Line ("Write to block " &
                (if Req.Status = Block.Client.Ok then " succeeded" else " failed"));
+               Block.Client.Acknowledge (Client, Req);
             end if;
          end;
-         exit when handled;
       end loop;
+      Gnat.Io.Put_Line ("Writing finished.");
+      Gnat.Io.Put_Line ("Reading...");
       Read_Req.Start := 1;
       Read_Req.Length := 1;
       Block.Client.Submit_Read (Client, Read_Req);
@@ -51,31 +56,26 @@ package body Ada_Block_Test is
       Block.Client.Submit_Read (Client, Read_Req);
       Read_Req.Start := 3;
       Block.Client.Submit_Read (Client, Read_Req);
-      started := False;
-      handled := false;
-      loop
+      Acknowledged_Blocks := 0;
+      while Acknowledged_Blocks < 3 loop
          declare
             Req : Block.Client.Request := Block.Client.Next (Client);
          begin
-            started := started or Req.Kind /= Block.Client.None;
-            handled := started and Req.Kind = Block.Client.None;
-            if handled then
-               Gnat.Io.Put_Line ("reading finished");
-            end if;
-            if started and not handled and Req.Kind = Block.Client.Read then
+            if Req.Kind = Block.Client.Read then
+               Acknowledged_Blocks := Acknowledged_Blocks + 1;
                if Req.Status = Block.Client.Ok then
                   Block.Client.Read (Client, Req, Buf);
                end if;
                Gnat.Io.Put_Line ("Reading from block " &
                (if Req.Status = Block.Client.Ok then " succeeded" else " failed"));
                if Req.Status = Block.Client.Ok then
-                  Gnat.Io.Put_Line (Convert_Block (Buf));
+                  Gnat.Io.Put_Line (Convert_Block (Buf) (1 .. Standard.Integer (Block_Size)));
                end if;
                Block.Client.Acknowledge (Client, Req);
             end if;
          end;
-         exit when handled;
       end loop;
+      Gnat.Io.Put_Line ("Reading finished.");
       Block.Client.Finalize_Device (Client);
    end Run;
 
--- src/cbe/test/block_client/cxx_block_test.cc
+++ src/cbe/test/block_client/cxx_block_test.cc
@@ -5,30 +5,35 @@ Cxx_block_test::Cxx_block_test() : _block()
 
 void Cxx_block_test::run()
 {
+    Genode::memset(_buffer, '\0', sizeof(_buffer));
     _block.initialize();
+    Genode::log("Block device with ", _block.block_count(), " blocks of size ", _block.block_size());
+    if(_block.block_size() > 4096)
+    {
+        Genode::error("Block size too big");
+    }
+    Genode::log("Writing...");
     Block::Client::Request req {Block::Client::WRITE, {}, 1, 1};
-    Genode::memset(_buffer, 'c', Block::Client::BLOCK_SIZE);
+    Genode::memset(_buffer, 'c', _block.block_size());
     req.start = 1;
-    _block.submit_write(req, (Genode::uint8_t *)_buffer, Block::Client::BLOCK_SIZE);
-    Genode::memset(_buffer, '+', Block::Client::BLOCK_SIZE);
+    _block.submit_write(req, (Genode::uint8_t *)_buffer, _block.block_size());
+    Genode::memset(_buffer, '+', _block.block_size());
     req.start = 2;
-    _block.submit_write(req, (Genode::uint8_t *)_buffer, Block::Client::BLOCK_SIZE);
-    Genode::memset(_buffer, '+', Block::Client::BLOCK_SIZE);
+    _block.submit_write(req, (Genode::uint8_t *)_buffer, _block.block_size());
+    Genode::memset(_buffer, '+', _block.block_size());
     req.start = 3;
-    _block.submit_write(req, (Genode::uint8_t *)_buffer, Block::Client::BLOCK_SIZE);
-    bool handled = false;
-    bool started = false;
-    while(!handled){
+    _block.submit_write(req, (Genode::uint8_t *)_buffer, _block.block_size());
+    int acked_block = 0;
+    while(acked_block < 3){
         req = _block.next();
-        started = started || req.kind != Block::Client::NONE;
-        handled = started && req.kind == Block::Client::NONE;
-        if(handled){
-            Genode::log("writing finished");
-        }
-        if(started && !handled){
+        if(req.kind == Block::Client::WRITE){
+            acked_block++;
             Genode::log("Write to block ", req.start, req.status == Block::Client::OK ? " succeeded" : " failed");
+            _block.acknowledge(req);
         }
     }
+    Genode::log("Writing finished.");
+    Genode::log("Reading...");
     req.kind = Block::Client::READ;
     req.start = 1;
     req.length = 1;
@@ -37,25 +42,21 @@ void Cxx_block_test::run()
     _block.submit_read(req);
     req.start = 3;
     _block.submit_read(req);
-    handled = false;
-    started = false;
-    while(!handled){
+    acked_block = 0;
+    while(acked_block < 3){
         req = _block.next();
-        started = started || req.kind != Block::Client::NONE;
-        handled = started && req.kind == Block::Client::NONE;
-        if(handled){
-            Genode::log("reading finished");
-        }
-        if(started && !handled){
+        if(req.kind == Block::Client::READ){
+            acked_block++;
             if(req.status == Block::Client::OK){
-                _block.read(req, (Genode::uint8_t *)_buffer, Block::Client::BLOCK_SIZE);
+                _block.read(req, (Genode::uint8_t *)_buffer, _block.block_size());
             }
             Genode::log("Reading from block ", req.start, req.status == Block::Client::OK ? " succeeded" : " failed");
             if(req.status == Block::Client::OK){
-                Genode::log(Genode::String<Block::Client::BLOCK_SIZE>(static_cast<const char*>(_buffer)));
+                Genode::log(Genode::String<sizeof(_buffer)>(static_cast<const char*>(_buffer)));
             }
             _block.acknowledge(req);
         }
     }
+    Genode::log("Reading finished...");
     _block.finalize();
 }
