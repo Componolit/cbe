badb9ad83b652260037047ebc35b6700b0e5bf21
Johannes Kliemann <kliemann@componolit.com>
2019-03-07 15:05:53 +0100

    asynchronous write

--- src/cbe/test/block_proxy/cai-block-server.adb
+++ src/cbe/test/block_proxy/cai-block-server.adb
@@ -1,57 +1,97 @@
 with Cai.Block.Client;
+with Gnat.Io;
 
 package body Cai.Block.Server is
+
+   type Read_Entry is record
+      Free : Boolean;
+      Block : Id;
+      Length : Count;
+      Address : System.Address;
+   end record;
+
+   type Read_List is array (Integer range <>) of Read_Entry;
+
+   Queue : Read_List (1 .. 5) := (others => (True, 0, 0, System.Null_Address));
+
+   procedure Callback (D : in out Component.Block_Server_Device);
+
+   package Block_Client is new Cai.Block.Client (Component.Block_Server_Device, Callback);
+
+   procedure Callback (D : in out Component.Block_Server_Device)
+   is
+   begin
+      loop
+         declare
+            R : Request := Block_Client.Next (D.Client);
+         begin
+            case R.Kind is
+               when None =>
+                  exit;
+               when Write =>
+                  Acknowledge (D, R, D.Context);
+               when others =>
+                  null;
+            end case;
+         end;
+      end loop;
+   end Callback;
 
    procedure Initialize (D : out Component.Block_Server_Device; L : String; C : Context)
    is
    begin
       D.Context := C;
-      Cai.Block.Client.Initialize_Device (D.Client, L);
+      Block_Client.Initialize_Device (D.Client, L, D);
    end Initialize;
 
    procedure Finalize (D : in out Component.Block_Server_Device)
    is
    begin
-      Cai.Block.Client.Finalize_Device (D.Client);
+      Block_Client.Finalize_Device (D.Client);
    end Finalize;
 
    function Block_Count (D : Component.Block_Server_Device) return Count
    is
    begin
-      return Cai.Block.Client.Block_Count (D.Client);
+      return Block_Client.Block_Count (D.Client);
    end Block_Count;
 
    function Block_Size (D : Component.Block_Server_Device) return Size
    is
    begin
-      return Cai.Block.Client.Block_Size (D.Client);
+      return Block_Client.Block_Size (D.Client);
    end Block_Size;
 
    function Writable (D : Component.Block_Server_Device) return Boolean
    is
    begin
-      return Cai.Block.Client.Writable (D.Client);
+      return Block_Client.Writable (D.Client);
    end Writable;
 
    function Maximal_Transfer_Size (D : Component.Block_Server_Device) return Unsigned_Long
    is
    begin
-      return Cai.Block.Client.Maximal_Transfer_Size (D.Client);
+      return Block_Client.Maximal_Transfer_Size (D.Client);
    end Maximal_Transfer_Size;
 
-   procedure Read (D : in out Component.Block_Server_Device; B : out Buffer; R : in out Request)
+   procedure Read (D : in out Component.Block_Server_Device; B : System.Address; L : Unsigned_Long; R : in out Request)
    is
       Req : constant Request := R;
       Acked : Boolean := False;
    begin
-      Cai.Block.Client.Submit_Read (D.Client, Req);
+      Block_Client.Submit_Read (D.Client, Req);
       while not Acked loop
          declare
-            N : Request := Cai.Block.Client.Next (D.Client);
+            N : Request := Block_Client.Next (D.Client);
          begin
             if N.Kind = Cai.Block.Read then
                if N.Status = Cai.Block.Ok then
-                  Cai.Block.Client.Read (D.Client, N, B);
+                  declare
+                     Buf : Buffer (1 .. L)
+                     with Address => B;
+                  begin
+                     Block_Client.Read (D.Client, N, Buf);
+                  end;
                end if;
                R.Status := N.Status;
                Acknowledge (D, R, D.Context);
@@ -63,27 +103,16 @@ package body Cai.Block.Server is
 
    procedure Write (D : in out Component.Block_Server_Device; B : Buffer; R : in out Request)
    is
-      Req : constant Request := R;
       Acked : Boolean := False;
    begin
-      Cai.Block.Client.Submit_Write (D.Client, Req, B);
-      while not Acked loop
-         declare
-            N : constant Request := Cai.Block.Client.Next (D.Client);
-         begin
-            if N.Kind = Cai.Block.Write then
-               R.Status := N.Status;
-               Acknowledge (D, R, D.Context);
-               Acked := True;
-            end if;
-         end;
-      end loop;
+      Block_Client.Submit_Write (D.Client, R, B);
+      R.Status := Cai.Block.Ok;
    end Write;
 
    procedure Sync (D : in out Component.Block_Server_Device)
    is
    begin
-      Cai.Block.Client.Sync (D.Client);
+      Block_Client.Sync (D.Client);
    end Sync;
 
 end Cai.Block.Server;
--- src/cbe/test/block_proxy/cai-component.ads
+++ src/cbe/test/block_proxy/cai-component.ads
@@ -1,10 +1,10 @@
-with Cai.Block.Client;
+with Cai.Block;
 
 package Cai.Component is
 
    type Block_Server_Device is record
       Context : Cai.Block.Context;
-      Client : Cai.Block.Client.Device;
+      Client : Block.Device;
    end record;
 
 end Cai.Component;
