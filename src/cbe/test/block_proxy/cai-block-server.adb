6f3f42bfe5293d8538d027d030136579a52a525a
Johannes Kliemann <kliemann@componolit.com>
2019-03-07 15:26:26 +0100

    store and load read requests

--- src/cbe/test/block_proxy/cai-block-server.adb
+++ src/cbe/test/block_proxy/cai-block-server.adb
@@ -14,6 +14,36 @@ package body Cai.Block.Server is
 
    Queue : Read_List (1 .. 5) := (others => (True, 0, 0, System.Null_Address));
 
+   procedure Store (R : in out Request; A : System.Address)
+   is
+   begin
+      R.Status := Cai.Block.Error;
+      for I in Queue'Range loop
+         if Queue (I).Free then
+            R.Status := Cai.Block.Ok;
+            Queue (I).Free := False;
+            Queue (I).Block := R.Start;
+            Queue (I).Length := R.Length;
+            Queue (I).Address := A;
+            exit;
+         end if;
+      end loop;
+   end Store;
+
+   procedure Load (R : in out Request; A : out System.Address)
+   is
+   begin
+      R.Status := Cai.Block.Error;
+      for I in Queue'Range loop
+         if not Queue (I).Free and Queue (I).Block = R.Start and Queue (I).Length = R.Length then
+            A := Queue (I).Address;
+            R.Status := Ok;
+            Queue (I).Free := True;
+            exit;
+         end if;
+      end loop;
+   end Load;
+
    procedure Callback (D : in out Component.Block_Server_Device);
 
    package Block_Client is new Cai.Block.Client (Component.Block_Server_Device, Callback);
@@ -76,19 +106,21 @@ package body Cai.Block.Server is
 
    procedure Read (D : in out Component.Block_Server_Device; B : System.Address; L : Unsigned_Long; R : in out Request)
    is
-      Req : constant Request := R;
       Acked : Boolean := False;
+      Load_Addr : System.Address;
    begin
-      Block_Client.Submit_Read (D.Client, Req);
+      Store (R, B);
+      Block_Client.Submit_Read (D.Client, R);
       while not Acked loop
          declare
             N : Request := Block_Client.Next (D.Client);
          begin
             if N.Kind = Cai.Block.Read then
                if N.Status = Cai.Block.Ok then
+                  Load (N, Load_Addr);
                   declare
                      Buf : Buffer (1 .. L)
-                     with Address => B;
+                     with Address => Load_Addr;
                   begin
                      Block_Client.Read (D.Client, N, Buf);
                   end;
